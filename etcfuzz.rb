96fd7e84a6405ce6dee3d609c14b8429a6b699b5
Tyler Neely <t@jujit.su>
2015-06-01 22:00:48 -0700

    change fuzzer

--- etcfuzz.rb
+++ etcfuzz.rb
@@ -24,21 +24,13 @@ class EtcFuzz
         --initial-cluster="etcd-1=http://localhost:4001,etcd-2=http://localhost:4002,etcd-3=http://localhost:4003,etcd-4=http://localhost:4004" 
     ]
 
-    setup = [
-      Proc.new { || run 1, @setupInits[0], "new" },
-      Proc.new { ||
-        puts "trying"
-        run(2, @setupInits[1], "existing")
-      },
-      Proc.new { || run 3, @setupInits[2], "existing" },
-    ]
-    @threads[1] = Thread.new { setup.first.call }
-    [1,2].each { |i|
+    @threads[1] = Thread.new { run(1, @setupInits[0], "new") }
+    [2,3].each { |i|
       puts "waiting 3 seconds for it to come up"
       sleep 3
       @threads[i] = Thread.new do
-        configure i + 1
-        run i + 1, @setupInits[i], "existing"
+        configure i
+        run(i, @setupInits[i-1], "existing")
       end
     }
   end
@@ -54,23 +46,39 @@ class EtcFuzz
   end
 
   def configure i
-    puts "configuring #{i}"
-    r = `bin/etcdctl -C http://localhost:500#{@threads.keys.sample} member add etcd-#{i} http://localhost:400#{i}`
-    puts r
+    loop do
+      puts "configuring #{i}"
+      r = `bin/etcdctl -C http://localhost:500#{@threads.keys.sample} member add etcd-#{i} http://localhost:400#{i}`
+      puts "got #{r} of len #{r.length}"
+      break if r.length > 50
+      sleep 1 if r.length == 0
+    end
+  end
+
+  def deconfigure i
+    loop do
+      puts "configuring #{i}"
+      r = `bin/etcdctl -C http://localhost:500#{@threads.keys.sample} member remove #{i}`
+      puts "got #{r} of len #{r.length}"
+      break if r.length > 0
+      sleep 1 if r.length == 0
+    end
   end
 
   def run i, init, op="existing"
     `rm  -rf d#{i}`
     cmd = @invoke % {n: i, op: op, initial_cluster: init}
-    exit if cmd.include? "panic"
 
     puts "invoking", cmd
     result = system(cmd.split("\n").join(" "))
+    exit if result.include? "panic"
+    @threads.delete(i)
   end
 
   def kill_one
-    return unless @threads.length >= 2
+    return unless @threads.length > 2
     target = @threads.keys.sample
+    deconfigure get_port_to_ident[target]
     puts "running now: #{@threads.keys}"
     puts "trying to kill #{target}"
     Thread.kill(@threads[target])
@@ -81,17 +89,19 @@ class EtcFuzz
   def spawn_one
     return unless @threads.length < 4
     candidates = [1,2,3,4].reject { |e| @threads.keys.include? e }
+    puts "thread keys: #{@threads.keys}"
+    puts "candidates: #{candidates}"
     n = candidates.sample
     puts "running now: #{@threads.keys}"
     puts "trying to start #{n}"
     configure n
-    @threads[n] = Thread.new { || run n, @laterInits.sample }
+    @threads[n] = Thread.new { run(n, @laterInits.sample) }
   end
 end
 
 ef = EtcFuzz.new
 loop do
-  sleep 3
+  sleep 0.5
   if rand > 0.5
     ef.kill_one
   else
